976505817f226ccd0b800a9624058b543b78aa86
hariprasad <hprasad.sakhare@gmail.com>
2024-03-03 12:32:15 +0530

    first commit

---
+++ variableMe.ts
@@ -0,0 +1,16 @@
+let getting: string = "Hariprasad";
+
+getting.toLowerCase()
+console.log(getting);
+
+//number
+
+let userId: number = 334455
+userId.toFixed()
+
+//boolean
+let isLoggedIn: boolean = true
+
+
+
+export{}
