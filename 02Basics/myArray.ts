ea28d43caba356e08c7508c570b15985abf92af9
hariprasad <hprasad.sakhare@gmail.com>
2024-03-04 00:04:47 +0530

    first commit

---
+++ 02Basics/myArray.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+//const heroPower:number[] = []
+const heroPower:Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModel: number[][] = [
+    [225,335,355],
+    []
+]
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "Hariprasad", isActive: true})
