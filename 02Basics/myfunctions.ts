00fe9aaac10fc7cfd16fe85ac438843260b674d1
hariprasad <hprasad.sakhare@gmail.com>
2024-03-04 00:03:32 +0530

    first commit

--- 02Basics/myfunctions.ts
+++ 02Basics/myfunctions.ts
@@ -16,5 +16,29 @@ getUpper("hariprasad")
 signUpUser("hitesh","hiesah@cl.dev",false)
 loginUser("h","h@cl.dev")
 
+//function getValue(myVal: number):boolean{
+//    if(myVal > 5){
+//        return true
+//    }
+//    return "200 OK"
+// }
+
+const getHello =(s: string):string => {
+    return""
+}
+
+const heros = ["thor","spiderman","ironman"]
+//const heros = [1,2,3]
+heros.map((hero)=>{
+    return `hero is ${hero}`
+})
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg);
+}
+
+function handleError(errmsg: string): never{
+    throw new Error(errmsg)
+}
 
 export{} 
