9409a52bda3e5b65f9d7819a3604c08435c6bbb7
hariprasad <hprasad.sakhare@gmail.com>
2024-03-04 00:03:12 +0530

    first commit

--- 02Basics/myfunctions.js
+++ 02Basics/myfunctions.js
@@ -14,3 +14,23 @@ addTwo(5);
 getUpper("hariprasad");
 signUpUser("hitesh", "hiesah@cl.dev", false);
 loginUser("h", "h@cl.dev");
+//function getValue(myVal: number):boolean{
+//    if(myVal > 5){
+//        return true
+//    }
+//    return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+//const heros = [1,2,3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
