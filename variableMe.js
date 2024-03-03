1a0f2c6df21a700fdb56cd31e2656e20bb1cbd06
hariprasad <hprasad.sakhare@gmail.com>
2024-03-03 12:37:12 +0530

    first commit

--- variableMe.js
+++ variableMe.js
@@ -3,3 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var getting = "Hariprasad";
 getting.toLowerCase();
 console.log(getting);
+//number
+var userId = 334455;
+userId.toFixed();
+//boolean
+var isLoggedIn = true;
+//any
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
