063842542f4645046451d99461cf14cddb442a3e
hariprasad <hprasad.sakhare@gmail.com>
2024-03-04 00:04:38 +0530

    first commit

---
+++ 02Basics/myArray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+//const heroPower:number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModel = [
+    [225, 335, 355],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "Hariprasad", isActive: true });
