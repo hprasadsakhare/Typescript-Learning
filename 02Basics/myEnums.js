5a6134b296b1f1e31d4d857579c90102ffb35f66
hariprasad <hprasad.sakhare@gmail.com>
2024-03-04 00:05:11 +0530

    first commit

---
+++ 02Basics/myEnums.js
@@ -0,0 +1,8 @@
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice[SeatChoice["MIDDLE"] = 10] = "MIDDLE";
+    SeatChoice[SeatChoice["WINDOW"] = 11] = "WINDOW";
+    SeatChoice[SeatChoice["FOURTH"] = 12] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.AISLE;
