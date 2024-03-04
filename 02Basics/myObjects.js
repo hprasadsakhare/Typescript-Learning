de68bb01eba75104fa3d6f6b64a815ebfc2b9b7e
hariprasad <hprasad.sakhare@gmail.com>
2024-03-05 00:06:49 +0530

    first commit

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "Hariprasad",
+    email: "hari@lco.dev",
+    isActive: true
+};
+function crewateUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+crewateUser({ name: "hari", isPaid: false });
+function createCourse() {
+    return { name: "reactjs", isPaid: 399 };
+}
+var myUser = {
+    _id: "1245",
+    name: "hart",
+    email: "hari  @cl.dev",
+    isActive: false
+};
+myUser.email = "h@gmail.com";
