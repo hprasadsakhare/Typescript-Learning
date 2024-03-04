34d7922193120fb35a39057b785ac571a7b297b8
hariprasad <hprasad.sakhare@gmail.com>
2024-03-05 00:06:15 +0530

    first commit

---
+++ 02Basics/myInterface.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hitesh = { dbId: 22, name: "hitesh", email: "h@h.com", userId: 2211 };
+hitesh.email = "hitesh@cl.dev";
