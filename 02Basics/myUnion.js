0ff7b8f3569b6d2ac8e0912c0a3f71d7674afa39
hariprasad <hprasad.sakhare@gmail.com>
2024-03-06 19:09:23 +0530

    first commit

---
+++ 02Basics/myUnion.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var score = 33;
+score = 44;
+score = "55";
+var hari = { name: "Hariprasad", id: 334455 };
+hari = { username: "hprasad", id: 334455 };
+function getDbId(id) {
+    console.log("Db id is ".concat(id));
+}
+getDbId(22);
+getDbId("33");
+// array
+var data = [1, 2, 3];
+var data2 = ["1", "2", "3"];
+var data3 = ["1", 2, 3];
