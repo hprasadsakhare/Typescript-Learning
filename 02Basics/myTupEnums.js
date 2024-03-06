5d0d5ec898e82b973c3277e9b62e4af33a8d3c64
hariprasad <hprasad.sakhare@gmail.com>
2024-03-06 19:08:51 +0530

    first commit

---
+++ 02Basics/myTupEnums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//const user: string[] = ["hc",]
+var tUser;
+tUser = ["hc", 1, true];
+var rgb = [22, 123, 255];
+var newUser = [112, "example@cl.dev"];
+newUser[1] = "hc.com";
