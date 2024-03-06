3d16a6b24810254ef9e59a8d72fbe5bc1a59c4d4
hariprasad <hprasad.sakhare@gmail.com>
2024-03-06 19:09:04 +0530

    first commit

---
+++ 02Basics/myTupEnums.ts
@@ -0,0 +1,14 @@
+//const user: string[] = ["hc",]
+let tUser:[string,number,boolean]
+
+tUser = ["hc", 1, true]
+
+let rgb: [number,number,number] = [22,123, 255]
+
+type User = [number, string]
+const newUser: User = [112, "example@cl.dev"]
+
+newUser[1]= "hc.com"
+
+
+export{}
