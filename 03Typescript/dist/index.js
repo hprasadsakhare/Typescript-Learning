2f0c4d9126ebab735f2c1405f2b554403268c2fa
hariprasad <hprasad.sakhare@gmail.com>
2024-03-07 07:48:58 +0530

    first commit

--- 03Typescript/dist/index.js
+++ 03Typescript/dist/index.js
@@ -1,17 +1,4 @@
 "use strict";
-/*
-const obj: {
-    height: number;
-    weight: number;
-    gender: boolean;
-};
-
-const obj: Obj = {
-    height: 3434,
-    weight:32,
-};
-
-*/
 const func = (n, m, l) => {
     return n * m * l;
 };
--- 03Typescript/src/index.ts
+++ 03Typescript/src/index.ts
@@ -1,17 +1,3 @@
-/*
-const obj: {
-    height: number;
-    weight: number;
-    gender: boolean;
-}; 
-
-const obj: Obj = {
-    height: 3434,
-    weight:32,
-};
-
-*/
-
 type FuncType = (n:number,m:number,l:number) => number;
 
 const func: FuncType = (n,m,l) => {
