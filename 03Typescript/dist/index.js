2c485277b5a0b65bf0f18233df2c79479c2d7c19
hariprasad <hprasad.sakhare@gmail.com>
2024-03-08 12:30:48 +0530

    first commit

--- 03Typescript/dist/index.js
+++ 03Typescript/dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
-const func = (n, m, l) => {
-    return n * m * l;
+/*
+type FuncType = (n:number,m:number,l:number) => number;
+
+const func: FuncType = (n,m,l) => {
+    return n * m * l
 };
-func(23, 32, 43);
+
+func(23,32,43)
+*/
+const func = (n, m) => {
+    if (typeof 1 === "undefined")
+        return n * m;
+    return n * m;
+};
+func(25, 23);
--- 03Typescript/src/index.ts
+++ 03Typescript/src/index.ts
@@ -1,3 +1,4 @@
+/*
 type FuncType = (n:number,m:number,l:number) => number;
 
 const func: FuncType = (n,m,l) => {
@@ -5,3 +6,13 @@ const func: FuncType = (n,m,l) => {
 };
 
 func(23,32,43)
+*/
+
+type FuncType = (n:number, m:number, ) => number;
+
+const func: FuncType = (n,m,) => {
+    if(typeof 1 ==="undefined") return n*m;
+    return n*m;
+};
+
+func(25,23);
